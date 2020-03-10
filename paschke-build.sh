39e7415c50dd36c7b81360bebb75b0ad1126966d
Paul David <paul@denknerd.org>
2020-03-10 19:43:24 +1100

    I can partition!

--- paschke-build.sh
+++ paschke-build.sh
@@ -6,6 +6,23 @@ set -o pipefail
 
 echo "Welcome to Paschke.  We'll try and build a Windows 95 image here."
 
+wd=$(mktemp --directory)
+
+if [ -f "win95.disk" ]; then
+    echo "Oops, vdisk already exists."
+    exit 3
+fi
+
+if [ ! -f "win95.disk" ]; then
+    qemu-img create -f qcow win95.disk 250M
+fi
+
+
+./partition.expect
+# qemu-system-i386 -hda win95.disk -m 100 -boot a -vga std -usb -fda 622C.IMG -nographic
+
+
+
 # Proposed steps:
 # somehow make a qemu image. e.g., 300MB
 # give it one primary partitition, make it boot/"active"
