788c0afedaf4ff8b231645445a383ee1a12620a5
Paul David <paul@denknerd.org>
2020-03-10 21:15:58 +1100

    Remove DONE steps.

--- paschke-build.sh
+++ paschke-build.sh
@@ -43,19 +43,10 @@ qemu-system-i386 -drive file=win95_disk.img,format=raw -m 100 -boot a -vga std -
 
 
 # Proposed steps:
-# somehow make a qemu image. e.g., 300MB
-# give it one primary partitition, make it boot/"active"
-# format it as FAT16 or FAT32 (OSS tools exist??)
-# mount / load the hdd image for r/w
-# find a win95 ISO
-# Mount / load the CD image
-# extract the /WIN95 folder into the hdd's FAT partition
 
 # find a msbatch.inf?? file somewhere
 # add it to the /WIN95 folder
 
-# find a dos 622C.IMG floppy, add an AUTOEXEC.BAT to it with contents:
-#     C: ; CD WIN95 ; SETUP /IS /IW MSBATCH.INF
 
 # Try boot it all up in qemu i386???
 
