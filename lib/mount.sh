6d7636367a755baa7942d72e6d5f7cd7869ddd47
Paul David <paul@denknerd.org>
2020-03-14 20:28:27 +1100

    Make it work in Linux 👌

--- lib/mount.sh
+++ lib/mount.sh
@@ -4,20 +4,69 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-function mount() {
-    # This function mounts an image and returns the mount point.
-
-    if [ ! -f "$1" ]; then
-        echo "Cannot find image $1!" 1>&2
-        exit 2
-    fi
-
+function _macos_mount() {
     local output
     output=$(hdiutil attach "$1")
 
     echo "$output" | sed -n 's#.*\(/Volume.*\)$#\1#p'
 }
 
-function eject() {
+function _linux_mount() {
+    local mountPoint
+    mountPoint=$(mktemp --directory)
+
+	mount_ro="${2:-}"
+	if [ "${mount_ro}" = "ro" ]; then
+		options=""
+    else
+		options="-o umask=000"
+	fi
+	loopback=$(sudo losetup --partscan --show --find "$1")
+
+	# Ugh, we don't always have partitions...
+	if [ -e "${loopback}p1" ]; then
+			loopback="${loopback}p1"
+	fi
+	echo "Mounting ${loopback} on ${mountPoint}..." 1>&2
+	sudo mount "${loopback}" "${mountPoint}" $options
+
+    echo "$mountPoint" 
+}
+
+function image_mount() {
+    # This function mounts an image and returns the mount point.
+    if [ ! -f "$1" ]; then
+        echo "Cannot find image $1!" 1>&2
+        exit 2
+    fi
+    
+    if [ "$(uname -s)" = "darwin" ]; then
+		_macos_mount "$@"
+    fi
+    if [ "$(uname -s)" = "Linux" ]; then
+		_linux_mount "$@"
+    fi
+}
+
+function _linux_eject() {
+	# magic! TODO find correct loopback and detach
+	echo "Unmounting ${1}..."
+	lodev=$(mount | grep "${1}" | awk '{print $1}')
+	sudo umount "$1"
+
+	if [[ "$lodev" =~ loop[0-9]p[0-9]$ ]]; then
+		# Need to strip off partition number...
+		lodev=$(echo -n "$lodev" | sed 's/p[0-9]$//')
+	fi
+	echo "Detaching ${lodev}..."
+	sudo losetup --detach "${lodev}"
+}
+
+function _macos_eject() {
     hdiutil eject "$1"
 }
+
+function eject() {
+	_linux_eject "$@"
+}
+
--- paschke-build.sh
+++ paschke-build.sh
@@ -14,27 +14,40 @@ if [ -f "win95_disk.img" ]; then
 fi
 
 if [ ! -f "win95_disk.img" ]; then
-    qemu-img create -f raw win95_disk.img 512M
+    qemu-img create -f raw win95_disk.img 400M
 fi
 
 # We keep a 'golden' image, because it's mounted RW, so who knows
 # what'll happen to it.
-gunzip --keep --force images/622C.IMG.golden.gz
-mv images/622C.IMG.golden images/622C.IMG
+gunzip --keep --force images/win95b-boot.img.gz
 
-# the MS-DOS 6.22 floppy image
-dos_boot_mountpoint=$(mount images/622C.IMG)
-rm -v "$dos_boot_mountpoint/CONFIG.SYS"
-cp -v 01partition/FDISK.SCP "$dos_boot_mountpoint"
-cp -v 01partition/FDISK.BAT "$dos_boot_mountpoint"
-cp -v 01partition/FORMAT.SCP "$dos_boot_mountpoint"
-cp -v 01partition/FORMAT.BAT "$dos_boot_mountpoint"
-# This AUTOEXEC partitions and formats.  There's also one to kick off
-# the setup, but we stick it on the floppy later.
-cp -v 01partition/partition.AUTOEXEC.BAT "$dos_boot_mountpoint/AUTOEXEC.BAT"
-eject "$dos_boot_mountpoint"
+# Make a FAT partition on that thing
+sudo fdisk -u ./win95_disk.img > /dev/null <<EOF
+o
+n
+p
+1
 
-./01partition/partition.expect
+
+
+t
+c
+
+a
+
+w
+
+EOF
+
+# Now just show results
+sudo fdisk -lu ./win95_disk.img
+
+## Format as FAT32
+
+# Find a loopback device for it
+loopback=$(sudo losetup --partscan --show --find ./win95_disk.img)
+sudo mkfs.fat -F 16 -n TURNIP "${loopback}p1"
+sudo losetup --detach "${loopback}"
 
 echo ""
 echo "Partition and format done.  Will try prepare setup folder now."
@@ -42,13 +55,13 @@ echo "Partition and format done.  Will try prepare setup folder now."
 # We have to mount the MS-DOS 6.22 floppy image again, to add an
 # AUTOEXEC.BAT, which kicks off SETUP.EXE.  If we added it up above,
 # it wouldn't have done the formatting correctly.
-dos_boot_mountpoint=$(mount images/622C.IMG)
+dos_boot_mountpoint=$(image_mount images/win95b-boot.img)
 cp -v 02runsetup/runsetup.AUTOEXEC.BAT "$dos_boot_mountpoint/AUTOEXEC.BAT"
 eject "$dos_boot_mountpoint"
 
 # Grab actual mount points:
-hdd_mount=$(mount win95_disk.img) # our fresh HDD image
-win_cdrom=$(mount "images/Win95 OSR2.ISO") # The source CD
+hdd_mount=$(image_mount win95_disk.img) # our fresh HDD image
+win_cdrom=$(image_mount "images/Win95 OSR2.ISO" ro) # The source CD
 
 # Windows ISOs don't always have consistent capitalisation, so maybe
 # it's D:\WIN95, or maybe it's D:\win95 we need...
@@ -70,10 +83,11 @@ echo "Starting Windows setup run..."
 
 qemu-system-i386 -drive file=win95_disk.img,format=raw \
                  -cpu pentium \
+                 -d cpu_reset,unimp,guest_errors \
                  -m 100 \
                  -boot order=ca,once=a \
                  -vga std \
-                 -drive file=images/622C.IMG,format=raw,index=0,if=floppy \
+                 -drive file=images/win95b-boot.img,format=raw,index=0,if=floppy \
                  -nic none
 
 # Proposed steps:
