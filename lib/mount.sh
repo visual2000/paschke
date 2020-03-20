e71b55515e6983466a3e5c29fd7d7d829ad91679
Paul David <paul@denknerd.org>
2020-03-20 22:07:58 +1100

    lib/mount.sh: Untabify.

--- lib/mount.sh
+++ lib/mount.sh
@@ -15,20 +15,20 @@ function _linux_mount() {
     local mountPoint
     mountPoint=$(mktemp --directory)
 
-	mount_ro="${2:-}"
-	if [ "${mount_ro}" = "ro" ]; then
-		options=""
+    mount_ro="${2:-}"
+    if [ "${mount_ro}" = "ro" ]; then
+        options=""
     else
-		options="-o umask=000"
-	fi
-	loopback=$(sudo losetup --partscan --show --find "$1")
+        options="-o umask=000"
+    fi
+    loopback=$(sudo losetup --partscan --show --find "$1")
 
-	# Ugh, we don't always have partitions...
-	if [ -e "${loopback}p1" ]; then
-			loopback="${loopback}p1"
-	fi
-	echo "Mounting ${loopback} on ${mountPoint}..." 1>&2
-	sudo mount "${loopback}" "${mountPoint}" $options
+    # Ugh, we don't always have partitions...
+    if [ -e "${loopback}p1" ]; then
+        loopback="${loopback}p1"
+    fi
+    echo "Mounting ${loopback} on ${mountPoint}..." 1>&2
+    sudo mount "${loopback}" "${mountPoint}" $options
 
     echo "$mountPoint"
 }
@@ -41,25 +41,25 @@ function image_mount() {
     fi
 
     if [ "$(uname -s)" = "Darwin" ]; then
-		_macos_mount "$@"
+        _macos_mount "$@"
     fi
     if [ "$(uname -s)" = "Linux" ]; then
-		_linux_mount "$@"
+        _linux_mount "$@"
     fi
 }
 
 function _linux_eject() {
-	# magic! TODO find correct loopback and detach
-	echo "Unmounting ${1}..."
-	lodev=$(mount | grep "${1}" | awk '{print $1}')
-	sudo umount "$1"
+    # magic! TODO find correct loopback and detach
+    echo "Unmounting ${1}..."
+    lodev=$(mount | grep "${1}" | awk '{print $1}')
+    sudo umount "$1"
 
-	if [[ "$lodev" =~ loop[0-9]p[0-9]$ ]]; then
-		# Need to strip off partition number...
-		lodev=$(echo -n "$lodev" | sed 's/p[0-9]$//')
-	fi
-	echo "Detaching ${lodev}..."
-	sudo losetup --detach "${lodev}"
+    if [[ "$lodev" =~ loop[0-9]p[0-9]$ ]]; then
+        # Need to strip off partition number...
+        lodev=$(echo -n "$lodev" | sed 's/p[0-9]$//')
+    fi
+    echo "Detaching ${lodev}..."
+    sudo losetup --detach "${lodev}"
 }
 
 function _macos_eject() {
@@ -68,9 +68,9 @@ function _macos_eject() {
 
 function eject() {
     if [ "$(uname -s)" = "Darwin" ]; then
-		_macos_eject "$@"
+        _macos_eject "$@"
     fi
     if [ "$(uname -s)" = "Linux" ]; then
-		_linux_eject "$@"
+        _linux_eject "$@"
     fi
 }
